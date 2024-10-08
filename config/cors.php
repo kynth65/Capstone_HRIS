e1dc97af66106f432319dd78dcc4a511d985ed5e
kynth65 <kynth65@gmail.com>
2024-10-09 01:03:27 +0800

    add login in config

--- config/cors.php
+++ config/cors.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
+    'paths' => ['api/*', 'login', 'register', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
     'allowed_origins' => ['http://localhost:5173', "http://192.168.16.110:8000"], // React app URL
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
