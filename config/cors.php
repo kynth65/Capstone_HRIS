9b85c424f0bb1076c58a6ed670bde24d8cded66b
kynth65 <kynth65@gmail.com>
2024-10-09 01:30:25 +0800

    add in config allowed origins

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
 return [
     'paths' => ['api/*', 'login', 'register', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', "http://192.168.16.110:8000"], // React app URL
+    'allowed_origins' => ['https://gammacareservices.com', "http://192.168.16.110:8000"], // React app URL
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
