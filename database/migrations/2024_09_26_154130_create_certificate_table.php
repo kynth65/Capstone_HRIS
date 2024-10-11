54bc9ce2f15711040d2fae70baa9115880cea559
Kynth <kynth@gmail.com>
2024-10-12 02:10:06 +0800

    Incident Reports

--- database/migrations/2024_09_26_154130_create_certificate_table.php
+++ database/migrations/2024_09_26_154130_create_certificate_table.php
@@ -17,12 +17,13 @@ return new class extends Migration
             $table->string('certificate_name'); // Ensure this column exists
             $table->date('issued_date')->nullable(); // Change to nullable if necessary
             $table->date('expiring_date')->nullable(); // Ensure this column exists
-            $table->string('acertificate_file_path'); // Ensure this column exists
+            $table->string('certificate_file_path'); // Ensure this column exists
             $table->string('status')->default('Active');
             $table->string('type')->default('expirable');
             $table->string('category')->nullable();
             $table->boolean('is_archived')->default(false);
             $table->boolean('can_update')->default(false);
+            $table->string('created_by')->nullable();   
             $table->string('updated_by')->nullable();
             $table->timestamps();
         });
