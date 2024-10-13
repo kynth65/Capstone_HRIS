363a2ce9fefd99ce3189b10ed12892ec108aec7b
kynth65 <kynth65@gmail.com>
2024-10-13 16:18:12 +0800

    build

--- config/cors.php
+++ config/cors.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['http://localhost:5173', 'https://gammacareservices.com'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
