4db077893abce9989c4928306ca57207fc2c7863
kynth65 <kynth65@gmail.com>
2024-10-14 03:08:14 +0800

    change

--- app/Http/Controllers/Api/submitLeaveRequest.php
+++ app/Http/Controllers/Api/submitLeaveRequest.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
 
-class submitLeaveRequest extends Controller
+class SubmitLeaveRequest extends Controller
 {
     public function submitLeaveRequest(Request $request)
     {
