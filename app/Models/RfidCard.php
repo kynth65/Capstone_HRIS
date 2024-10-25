ccfdbff69cefb408f4222d67e1b8378d0b2cea9c
kynth65 <kynth65@gmail.com>
2024-10-25 18:36:24 +0800

    rfid

---
+++ app/Models/RfidCard.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RfidCard extends Model
+{
+    protected $fillable = [
+        'rfid_uid',
+        'status'
+    ];
+
+    protected $casts = [
+        'status' => 'string'
+    ];
+
+    // Scope to get available cards
+    public function scopeAvailable($query)
+    {
+        return $query->where('status', 'available');
+    }
+
+    // Scope to get assigned cards
+    public function scopeAssigned($query)
+    {
+        return $query->where('status', 'assigned');
+    }
+
+    // Scope to get inactive cards
+    public function scopeInactive($query)
+    {
+        return $query->where('status', 'inactive');
+    }
+}
